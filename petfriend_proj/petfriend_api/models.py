adac32ade55ae23c9c87538f768b75a878a91cc2
Wookie69Land <lukaskraj@gmail.com>
2023-05-14 16:04:12 +0200

    PF02 - models petfrienduser/pet NF

--- petfriend_proj/petfriend_api/models.py
+++ petfriend_proj/petfriend_api/models.py
@@ -13,7 +13,6 @@ class PetFriendUser(models.Model):
         return self.user.username
 
 
-
 class Pet(models.Model):
     name = models.CharField(max_length=128, unique=True)
     genre = models.SmallIntegerField(choices=((1, "boy"), (2, "girl")))
@@ -21,6 +20,9 @@ class Pet(models.Model):
     variety = models.CharField(max_length=128, null=True)
     birth_date = models.DateField(null=True)
     user = models.ForeignKey(PetFriendUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
     def get_age(self):
         today = date.today()
