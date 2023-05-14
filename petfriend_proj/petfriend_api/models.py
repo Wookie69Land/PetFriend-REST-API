934fa84c439391753e73bb8b6db3a51a5054ab92
Wookie69Land <lukaskraj@gmail.com>
2023-05-14 16:11:29 +0200

    PF03 - user model signals

--- petfriend_proj/petfriend_api/models.py
+++ petfriend_proj/petfriend_api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from datetime import date
 
@@ -11,6 +13,13 @@ class PetFriendUser(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        PetFriendUser.objects.create(user=instance)
+    instance.petfrienduser.save()
 
 
 class Pet(models.Model):
