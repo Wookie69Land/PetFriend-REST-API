aa557b5f4c854f00db123b4e4bbba50f769bd3d7
Lucas Land <lukaskraj@gmail.com>
2023-08-23 12:35:06 +0200

    basic py_client2

--- petfriend/py_client/basic.py
+++ petfriend/py_client/basic.py
@@ -1,9 +1,11 @@
 import requests
 
-endpoint = "https://dummyjson.com/products/1"
+test_endpoint = "https://dummyjson.com/products/1"
+endpoint = "http://localhost:8000/"
 
 if __name__ == "__main__":
 
     response = requests.get(endpoint)
-    print(response.json())
+    print(response.status_code)
+    #print(response.json())
 
