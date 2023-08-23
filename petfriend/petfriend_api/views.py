bfe409e5e3aff162ff9ae0b5836210df49282945
Lucas Land <lukaskraj@gmail.com>
2023-08-23 14:17:26 +0200

    first simple view json

--- petfriend/petfriend_api/views.py
+++ petfriend/petfriend_api/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
 def api_home(request, *args, **kwargs):
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        print("Can't see JSON")
+    print(data.keys())
+    print(data)
     return JsonResponse({"message": "This is API test"})
--- petfriend/py_client/basic.py
+++ petfriend/py_client/basic.py
@@ -1,11 +1,12 @@
 import requests
 
 test_endpoint = "https://dummyjson.com/products/1"
-endpoint = "http://localhost:8000/api"
+endpoint = "http://localhost:8000/api/"
 
 if __name__ == "__main__":
 
-    response = requests.get(endpoint)
+    response = requests.get(endpoint, params={"abc": 1234}, json={"echo": "Hello from the other side"})
     print(response.status_code)
     print(response.json())
+    print(response)
 
