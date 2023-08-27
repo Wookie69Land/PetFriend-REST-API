0757bfaed44e81d60d366b3199f9ebd37f012dca
Lukasz Krajewski <104900820+Wookie69Land@users.noreply.github.com>
2023-08-27 20:45:59 +0200

    Update base.py restframework

--- petfriend/settings/base.py
+++ petfriend/settings/base.py
@@ -125,6 +125,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+# RESTFramewok
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
